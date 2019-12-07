9da3f56b455812944254719d2242f671bbd1cb11
Maxim Antonov <57999732+Qaralle@users.noreply.github.com>
2019-12-07 19:36:04 +0300

    Add files via upload

---
+++ lab0pravki.sh
@@ -0,0 +1,115 @@
+mkdir lab0
+cd lab0
+touch amoonguss2
+touch blastoise8
+touch duglett3
+mkdir drapion8
+cd drapion8
+mkdir tympole
+touch grumpig
+mkdir gabite
+mkdir burmy
+mkdir shieldon
+cd ..
+mkdir hoppip0
+cd hoppip0
+mkdir tympole
+touch lampent 
+mkdir sceptile
+cd ..
+mkdir lickitung9
+cd lickitung9
+mkdir vaporeon
+touch wormadam
+mkdir spearow
+cd ..
+echo -e "Ходы After You Foul Play Gastro Acid Giga Drain Seed Bomb\nSleep Talk Snore Synthesis Worry Seed" > amoonguss2
+echo -e "Ходы Aqua Tail\nAvalanche Body Slam Brine Counter Defense Curl Dive Double-Edge\nDynamicpunch Flash Cannont Focus Punch Hydro Cannon Ice Punch Icy Wind\nIron Defense Iron Tail Mega Kick Mega Punch Hud-Slap Out-rage Rollout\nSeismic Toss Signal Beam Sleep Talk Snore Water Pledge Zen\nHeadbutt" > blastoise8
+echo -e "Возможности Overland=7 Burrow=8 Power=1\nIntelligence=3" > diglett3
+cd drapion8
+echo -e "Возможности Overland=6 Surface=6 Jump=6\nPower=3 IntelLigence=4" > grumpig
+cd ..
+cd hoppip0
+echo -e "Развитые способности Shadow\nTag" > lampent
+cd ..
+cd lickitung9
+echo -e "Возможности Overland=6 Surface=2 Jump=3 Power=1\nIntelligence=4 Threaded=0" > wormadam
+cd ..
+chmod 046 amoonguss2
+chmod 004 blastoise8
+chmod 046 diglett3
+chmod 736 drapion8
+cd drapion8
+chmod 315 tympole
+chmod 444 grumpig
+chmod 711 gabite
+chmod 511 burmy
+chmod 375 shieldon
+chmod 
+cd ..
+chmod 733 hoppip0
+cd hoppip0
+chmod 764 tympole
+chmod 666 lampent
+chmod 317 sceptile
+cd ..
+chmod 337 lickitung9
+cd lickitung9
+chmod 357 vaporeon
+chmod 622 wormadam
+chmod 315 spearow
+cd ..
+echo "lol working"
+chmod -R 777 drapion8
+chmod 777 lickitung9/spearow
+cp -r drapion8 ~/lab0/lickitung9/spearow
+chmod 736 drapion8
+cd drapion8
+chmod 315 tympole
+chmod 444 grumpig
+chmod 711 gabite
+chmod 511 burmy
+chmod 375 shieldon
+cd ..
+chmod 777 amoonguss2
+cat amoonguss2 > ~/lab0/hoppip0/lampentamoonguss
+chmod 046 amoonguss2
+cd hoppip0
+rm lampentamoonguss
+cd ..
+ln amoonguss2 ~/lab0/hoppip0/lampentamoonguss
+cat ~/lab0/lickitung9/wormadam ~/lab0/lickitung9/wormadam > ~/lab0/blastoise8_83 
+chmod 777 diglett3
+chmod 777 ~/lab0/drapion8/burmy
+cp diglett3 ~/lab0/drapion8/burmy
+chmod 046 diglett3
+chmod 511 ~/lab0/drapion8/burmy
+ln -s ~/lab0/diglett3 ~/lab0/drapion8/grumpigdiglett
+ln -s ~/lab0/hoppip0 Copy_57
+mkdir tmp
+(wc -c ~/lab0/drapion8/grumpig ~/lab0/hoppip0/lampent | sort -n) 2>/dev/null
+ls -lR | sort -k5 | tail -2
+grep -v "Me" ~/lab0/drapion8/grumpig ~/lab0/hoppip0/lampent 2>~/lab0/tmp/err
+ls -l `grep -l 'la' * */*` | sort -k5
+(wc -l  `echo l* */l*` | sort -k5)2>&1
+(ls -lR|grep "n$"| sort -k5) 2>>/tmp/err
+chmod 700 blastoise8
+rm blastoise8
+rm ./lickitung9/wormadam
+chmod 700 Copy_*
+rm Copy_*
+chmod 700 ./hoppip0/lampentamoongu*
+rm ./hoppip0/lampentamoongu*
+chmod 700 hoppip0
+rm -r hoppip0
+chmod 700 ./lickitung9/spearow
+chmod 700 ./lickitung9/spearow/drapion8
+chmod 700 ./lickitung9/spearow/drapion8/burmy
+chmod 700 ./lickitung9/spearow/drapion8/gabite
+chmod 700 ./lickitung9/spearow/drapion8/grumpig
+chmod 700 ./lickitung9/spearow/drapion8/shieldon
+rm -r ./lickitung9/spearow/drapion8
+rmdir ./lickitung9/spearow
+
+
+
